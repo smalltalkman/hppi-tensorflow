1685cae7f79337c13c1601dafca880d815500d65
smalltalkman <gui_yuan_miao@163.com>
2018-01-30 14:54:54 +0800

    generate binary file

--- 09-to-final.py
+++ 09-to-final.py
@@ -1,4 +1,4 @@
-import os
+import os, struct
 
 # Current work directory
 CWD = os.getcwd()
@@ -27,7 +27,7 @@ def generate_total_data_file(CT_INPUT_FILE, AC_INPUT_FILE, OUTPUT_FILE):
         ct_line=ct_line.strip()
         ac_line=ac_line.strip()
         if ct_line and ac_line:
-            output_line = ct_line + " " + ac_line + os.linesep
+            output_line = ct_line + " " + ac_line + '\n'
             output_file.write(output_line)
         if index%1000 == 0:
             print("finish index==" + str(index//1000))
@@ -52,11 +52,11 @@ def split_data_to_file():
         line=line.strip()
         if line:
             if index<=30000:
-                hppids_train_ppis.write(line+os.linesep)
-                hppids_train_labels.write("1 0"+os.linesep)
+                hppids_train_ppis.write(line+'\n')
+                hppids_train_labels.write("1 0"+'\n')
             else:
-                hppids_test_ppis.write(line+os.linesep)
-                hppids_test_labels.write("1 0"+os.linesep)
+                hppids_test_ppis.write(line+'\n')
+                hppids_test_labels.write("1 0"+'\n')
         if index%1000 == 0:
             print("finish index==" + str(index//1000))
         index = index + 1
@@ -71,11 +71,11 @@ def split_data_to_file():
         line=line.strip()
         if line:
             if index<=30000:
-                hppids_train_ppis.write(line+os.linesep)
-                hppids_train_labels.write("0 1"+os.linesep)
+                hppids_train_ppis.write(line+'\n')
+                hppids_train_labels.write("0 1"+'\n')
             else:
-                hppids_test_ppis.write(line+os.linesep)
-                hppids_test_labels.write("0 1"+os.linesep)
+                hppids_test_ppis.write(line+'\n')
+                hppids_test_labels.write("0 1"+'\n')
         if index%1000 == 0:
             print("finish index==" + str(index//1000))
         index = index + 1
@@ -87,10 +87,43 @@ def split_data_to_file():
     hppids_test_ppis.close()
     hppids_test_labels.close()
 
-if __name__=="__main__":
+def convert_txt_to_bin(txt_file_name, bin_file_name, rows, columns):
+    print("Convert "+txt_file_name+" to "+bin_file_name+" ...")
+    txt_file = open(txt_file_name)
+    bin_file = open(bin_file_name, 'wb')
+    bytes = struct.pack('ii', rows, columns)
+    bin_file.write(bytes)
+    bytes_len = len(bytes)
+    index = 1
+    line = txt_file.readline()
+    while line:
+        line=line.strip()
+        if line:
+            strs = line.split(" ")
+            floats = [float(s) for s in strs]
+            for f in floats:
+                bytes = struct.pack('f',f)
+                bin_file.write(bytes)
+                bytes_len = bytes_len + len(bytes)
+        if index%1000 == 0:
+            print("finish index==" + str(index//1000))
+        index = index + 1
+        line = txt_file.readline()
+    print("Converted bytes length= "+str(bytes_len))
+    txt_file.close()
+    bin_file.close()
+
+def main():
     for FILE, _ in FILES:
         print("Processing "+FILE+" ...")
         generate_total_data_file(CTDD+"/"+FILE, ACDD+"/"+FILE, FDD+"/"+FILE)
         print("Processed!")
     split_data_to_file()
+    convert_txt_to_bin(CWD+"/data/09-hppids/hppids-train-ppis.txt",   CWD+"/data/09-hppids/hppids-train-ppis.bin",   60000, 1106)
+    convert_txt_to_bin(CWD+"/data/09-hppids/hppids-train-labels.txt", CWD+"/data/09-hppids/hppids-train-labels.bin", 60000, 2)
+    convert_txt_to_bin(CWD+"/data/09-hppids/hppids-test-ppis.txt",    CWD+"/data/09-hppids/hppids-test-ppis.bin",    12915, 1106)
+    convert_txt_to_bin(CWD+"/data/09-hppids/hppids-test-labels.txt",  CWD+"/data/09-hppids/hppids-test-labels.bin",  12915, 2)
     print("Finished!!!")
+
+if __name__=="__main__":
+    main()
