aa3867ed82e82cde8acb81511288bd96f763c8de
smalltalkman <gui_yuan_miao@163.com>
2018-01-30 21:13:27 +0800

    recode hppi.py using oo-style

--- hppi.py
+++ hppi.py
@@ -1,50 +1,66 @@
 '''Load hppi train/test datas and labels
 '''
 
-__all__ = ['input_data']
+__all__ = ['read_data_sets']
 
-import os
+import struct
 from numpy import *
 
-FILES = {
-    "train_datas" : "hppids-train-ppis.txt",
-    "train_labels": "hppids-train-labels.txt",
-    "test_datas"  : "hppids-test-ppis.txt",
-    "test_labels" : "hppids-test-labels.txt",
-}
+class DataFile:
+    def __init__(self, file_path, file_name):
+        self.file_path = file_path
+        self.file_name = file_name
+        self.file = "%s/%s" % (self.file_path, self.file_name)
+        self.head_len = 8
+        self.head_fmt = 'ii'
+        with open(self.file, 'rb') as file:
+            (self.rows, self.columns) = struct.unpack(self.head_fmt, file.read(self.head_len))
+        self.item_len = 4
+        self.item_fmt = "%if"
+    def data_len(self, row_count=1):
+        return row_count * self.columns * self.item_len
+    def data_fmt(self, row_count=1):
+        return self.item_fmt % (row_count * self.columns)
+    def data_index(self, index=0):
+        return self.head_len + self.data_len(index)
+    def row(self, row_start=0, row_count=1):
+        with open(self.file, 'rb') as file:
+            file.seek(self.data_index(row_start))
+            datas = struct.unpack(self.data_fmt(row_count), file.read(self.data_len(row_count)))
+        return array(datas, dtype=float32).reshape(row_count, self.columns)
 
-def load_datas(dir, key):
-    datas = []
-    filepath = dir + "/" + FILES[key]
-    print("Loading data from " + filepath + " ...")
-    file = open(filepath)
-    index = 1
-    line = file.readline()
-    while line:
-        line = line.strip()
-        if line:
-            strs = line.split(" ")
-            floats = [float(s) for s in strs]
-            datas.append(floats)
-        if index%1000 == 0:
-            print("finish index==" + str(index//1000))
-        index = index + 1
-        line = file.readline()
-    file.close()
-    return datas
+class DataSet:
+    def __init__(self, file_path, datas_file_name, labels_file_name):
+        self.file_path = file_path
+        self.datas_file_name = datas_file_name
+        self.datas_data_file = DataFile(file_path, datas_file_name)
+        self.labels_file_name = labels_file_name
+        self.labels_data_file = DataFile(file_path, labels_file_name)
+        if self.datas_data_file.rows > self.labels_data_file.rows:
+            self.length = self.labels_data_file.rows
+        else:
+            self.length = self.datas_data_file.rows
+        self.batch_index = 0
+    @property
+    def datas(self):
+        return self.datas_data_file.row(0, self.datas_data_file.rows)
+    @property
+    def labels(self):
+        return self.labels_data_file.row(0, self.labels_data_file.rows)
+    def next_batch(self, length):
+        if self.batch_index + length > self.length:
+            self.batch_index = 0
+        datas = (self. datas_data_file.row(self.batch_index, length),
+                 self.labels_data_file.row(self.batch_index, length),
+                 )
+        self.batch_index = self.batch_index + length
+        return datas
 
-def input_data(dir):
-    train_datas  = array(load_datas(dir, "train_datas"),  dtype=float32)
-    train_labels = array(load_datas(dir, "train_labels"), dtype=float32)
-    test_datas   = array(load_datas(dir, "test_datas"),   dtype=float32)
-    test_labels  = array(load_datas(dir, "test_labels"),  dtype=float32)
-    datas = {
-        "train":{ "datas":train_datas, "labels":train_labels },
-        "test" :{ "datas":test_datas,  "labels":test_labels  },
-    }
-    return datas
+class DataSets:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.train = DataSet(file_path, "hppids-train-ppis.bin", "hppids-train-labels.bin")
+        self.test  = DataSet(file_path, "hppids-test-ppis.bin",  "hppids-test-labels.bin" )
 
-if __name__=="__main__":
-    dir = os.getcwd() + "/data/09-hppids"
-    global hppids
-    hppids = input_data(dir)
+def read_data_sets(file_path):
+    return DataSets(file_path)
