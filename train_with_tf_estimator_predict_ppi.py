e4a71575ab314d7b3793add33be64a121f1024be
smalltalkman <gui_yuan_miao@163.com>
2018-05-19 10:15:08 +0800

    train hppi model using mos0 code method

--- train_with_tf_estimator_predict_ppi.py
+++ train_with_tf_estimator_predict_ppi.py
@@ -130,6 +130,7 @@ def main():
     # more(times, cwd+"/data/09-hppids", "ct+ac", 1106, hidden_units, activation_fn, 2, optimizer, learning_rate, dnn_info, num_steps, model_dir_root, result_dir_root)
     # more(times, cwd+"/data/04-ld-bin",    "ld", 1260, hidden_units, activation_fn, 2, optimizer, learning_rate, dnn_info, num_steps, model_dir_root, result_dir_root)
     # more(times, cwd+"/data/05-mos-bin",  "mos",   98, hidden_units, activation_fn, 2, optimizer, learning_rate, dnn_info, num_steps, model_dir_root, result_dir_root)
+    # more(times, cwd+"/data/06-mos0-bin","mos0",  800, hidden_units, activation_fn, 2, optimizer, learning_rate, dnn_info, num_steps, model_dir_root, result_dir_root)
 
 if __name__ == "__main__":
     main()
