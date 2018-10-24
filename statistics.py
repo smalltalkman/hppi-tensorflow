727892934d8c3d14af703d2eb946ffe1562fc7ec
Yuanmiao Gui <gui_yuan_miao@163.com>
2018-10-24 12:34:41 +0800

    update statistics

--- statistics.py
+++ statistics.py
@@ -23,17 +23,36 @@ def statistics(INPUT_FILE, OUTPUT_FILE):
     # unique
     values=list(set(values))
     # statistics
-    statistics_data={}
+    # statistics_data={}
+    intervals = [[  50,          100, 0]
+                ,[ 100,          200, 0]
+                ,[ 200,          300, 0]
+                ,[ 300,          400, 0]
+                ,[ 400,          500, 0]
+                ,[ 500,         1000, 0]
+                ,[1000,         2000, 0]
+                ,[2000,         6000, 0]
+                ,[6000, float('inf'), 0]
+                ]
     for index, value in enumerate(values):
         size=len(value)
-        if not statistics_data.has_key(size):
-            statistics_data[size]=1
-        else:
-            statistics_data[size]=statistics_data[size]+1
+        # if not statistics_data.has_key(size):
+            # statistics_data[size]=1
+        # else:
+            # statistics_data[size]=statistics_data[size]+1
+        for index, interval in enumerate(intervals):
+          start, end, count = interval
+          if start<=size and size<end:
+            intervals[index][2] = count+1
     # write to output_file
+    # with open(OUTPUT_FILE, 'w') as output_file:
+        # for size, count in statistics_data.items():
+            # line=str(size)+','+str(count)
+            # output_file.write(line+'\n')
     with open(OUTPUT_FILE, 'w') as output_file:
-        for size, count in statistics_data.items():
-            line=str(size)+','+str(count)
+        for index, interval in enumerate(intervals):
+            start, end, count = interval
+            line=str(start)+','+str(count)
             output_file.write(line+'\n')
 
 if __name__=="__main__":
