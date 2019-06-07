fb282c850d775dc0df16f34c888b0ba8e5f52fc1
Yuanmiao Gui <gui_yuan_miao@163.com>
2019-06-07 11:33:26 +0800

    examples: add keras_autoencoder.py

---
+++ examples/keras_autoencoder.py
@@ -0,0 +1,70 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+
+def create_autoencoder(input_dim, encoding_dim):
+  input   = Input(shape=(input_dim,))
+  encoded = Dense(encoding_dim, activation='relu')(input)
+  decoded = Dense(input_dim, activation='sigmoid')(encoded)
+
+  autoencoder = Model(input, decoded)
+  encoder     = Model(input, encoded)
+
+  encoded_input = Input(shape=(encoding_dim,))
+  decoder_layer = autoencoder.layers[-1]
+  decoder       = Model(encoded_input, decoder_layer(encoded_input))
+
+  return autoencoder, encoder, decoder
+
+from keras.datasets import mnist
+import numpy as np
+
+def load_datas():
+  (x_train, _), (x_test, _) = mnist.load_data()
+
+  x_train = x_train.astype('float32') / 255.
+  x_test  = x_test .astype('float32') / 255.
+
+  x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+  x_test  = x_test .reshape((len(x_test ), np.prod(x_test .shape[1:])))
+
+  # print x_train.shape
+  # print x_test.shape
+
+  return x_train, x_test
+
+autoencoder, encoder, decoder = create_autoencoder(784, 32)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+x_train, x_test = load_datas()
+
+autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+# encode and decode some digits
+# note that we take them from the *test* set
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+# use Matplotlib (don't ask)
+import matplotlib.pyplot as plt
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
