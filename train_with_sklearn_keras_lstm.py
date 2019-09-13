4fd49a3242eae0426671f82a5c99bcac6aec378b
Yuanmiao Gui <gui_yuan_miao@163.com>
2019-09-13 23:02:22 +0800

    fix: train_with_sklearn_keras_lstm.py

--- train_with_sklearn_keras_lstm.py
+++ train_with_sklearn_keras_lstm.py
@@ -4,7 +4,7 @@ SEED = 7
 import numpy
 numpy.random.seed(SEED)
 
-AMINO_ACIDS='*ACDEFGHIKLMNPQRSTVWY'
+AMINO_ACIDS='*-ACDEFGHIKLMNPQRSTVWY'
 AMINO_ACID_MAP={ AMINO_ACID : INDEX for (INDEX, AMINO_ACID) in enumerate(AMINO_ACIDS) }
 AMINO_ACID_DIM=len(AMINO_ACIDS)
 
@@ -16,12 +16,12 @@ import pandas
 max_length = 1000
 
 positive_datas = pandas.read_csv("data/01-flat/Supp-A-36630-HPRD-positive-interaction.txt", header=None)
-positive_datas['seq_pair'] = (positive_datas[0]+'*'+positive_datas[1]).map(to_int_list)
+positive_datas['seq_pair'] = (positive_datas[0]+'-'+positive_datas[1]).map(to_int_list)
 positive_datas['class'] = 1
 positive_datas = positive_datas[positive_datas['seq_pair'].map(len)<=max_length]
 
 negative_datas = pandas.read_csv("data/01-flat/Supp-B-36480-HPRD-negative-interaction.txt", header=None)
-negative_datas['seq_pair'] = (negative_datas[0]+'*'+negative_datas[1]).map(to_int_list)
+negative_datas['seq_pair'] = (negative_datas[0]+'-'+negative_datas[1]).map(to_int_list)
 negative_datas['class'] = 0
 negative_datas = negative_datas[negative_datas['seq_pair'].map(len)<=max_length]
 
@@ -32,7 +32,7 @@ y = all_datas['class']
 
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=SEED)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
 
 from keras.preprocessing import sequence
 
@@ -46,13 +46,17 @@ from keras.layers import Embedding
 
 emb_vecor_length = 256
 model = Sequential()
-model.add(Embedding(AMINO_ACID_DIM, emb_vecor_length, input_length=max_length))
+model.add(Embedding(AMINO_ACID_DIM, emb_vecor_length, mask_zero=True, input_length=max_length))
 model.add(LSTM(100))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
-model.fit(X_train, y_train, epochs=10, batch_size=64)
+history = model.fit(X_train, y_train, shuffle=True, validation_split=0.25, epochs=20, batch_size=64)
 
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
+
+import json
+with open('train_with_sklearn_keras_lstm.json', 'w') as file:
+  json.dump(history.history, file)
